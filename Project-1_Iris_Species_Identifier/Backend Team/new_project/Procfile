1861193d19789e90d46513bc1fb613d5210a57a8
Atharva <atharvakawale124421@gmail.com>
2022-03-12 20:16:26 +0530

    added new version of iris project and created basic backend for spam detection project

---
+++ Project-1_Iris_Species_Identifier/Backend Team/new_project/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn new_project.wsgi
--- Project-1_Iris_Species_Identifier/Backend Team/new_project/new_project/wsgi.py
+++ Project-1_Iris_Species_Identifier/Backend Team/new_project/new_project/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+from whitenoise import WhiteNoise
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'new_project.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(get_wsgi_application())
